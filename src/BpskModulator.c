fc1cb17f9a93556bc6fea497db6e5192302bbced
algerbrest <mounir_achir@yahoo.fr>
2021-06-10 18:23:51 +0200

    Integrate the BPSK Modulation Unit to the main ; the Unit is empty

---
+++ src/BpskModulator.c
@@ -0,0 +1,23 @@
+
+/* 
+	LDPC Codec Sim.
+	----------------------------------------------------------------------------
+	Bpsk Modulator      
+	----------------------------------------------------------------------------
+
+	08/06/2021	
+*/
+
+
+
+#include "../include/BpskModulator.h"
+
+void BpskModulator_Init(void)
+{
+printf("BpskModulator Initilized\n");
+}
+
+void BpskModulator_Runnable(void)
+{
+printf("BpskModulator Run \n");
+}
