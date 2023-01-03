bedfcb325fd0e053db7f05af6d40a3fcdca88b66
Mounir <mounir_achir@yahoo.fr>
2023-01-03 15:07:40 +0100

    AwgnChannel module update, header file mainly

--- include/AwgnChannel.h
+++ include/AwgnChannel.h
@@ -6,3 +6,22 @@
 
 	29/12/2022
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+#define SIZE_MODULATED_FRAME 576
+
+
+typedef  float   BIT;
+
+typedef struct AWGN_MFRAME
+{
+	BIT bits[SIZE_MODULATED_FRAME];		// Static table containing bits 
+} AWGN_MODULATED_FRAME;
+
+void AwgnChannel_Init(AWGN_MODULATED_FRAME*);
+
+void AwgnChannel_Runnable(void);
--- src/AwgnChannel.c
+++ src/AwgnChannel.c
@@ -7,8 +7,11 @@
 	29/12/2022
 */
 
+#include"../include/AwgnChannel.h"
 
-void AwgnChannel_Init(void)
+
+
+void AwgnChannel_Init(AWGN_MODULATED_FRAME* awgnmf)
 {
 
 }
