b57cd8ad9e308484cbc450e982fe2daaa1e3a5a1
Mounir <mounir_achir@yahoo.fr>
2023-01-22 20:56:56 +0100

    BPSK Demodulator header file ; init function.

--- include/BpskDemodulator.h
+++ include/BpskDemodulator.h
@@ -1,1 +1,28 @@
-#pragma once
+/*
+	LDPC Codec Sim.
+	----------------------------------------------------------------------------
+	BPSK Demod.
+	----------------------------------------------------------------------------
+
+	22/01/2023
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+#define SIZE_DEMODULATED_FRAME 576
+
+
+typedef  float   BIT;
+
+typedef struct DMFRAME
+{
+	BIT bits[SIZE_DEMODULATED_FRAME];		// Static table containing bits 
+} DEMODULATED_FRAME;
+
+
+void BpskDemodulator_Init(DEMODULATED_FRAME*);
+
+void BpskDemodulator_Runnable(void);
