68ce6b14805b2c2126fe5bb01468ac64516a99b2
Mounir <mounir_achir@yahoo.fr>
2023-04-10 16:38:09 +0200

    Add LdpcDecoder.c + H matrix (Parity Check Matrix; used for the deocding).

---
+++ src/LdpcDecoder.c
@@ -0,0 +1,18 @@
+
+/*
+	LDPC Codec Sim.
+	----------------------------------------------------------------------------
+	LDPC Decoder - BPA : Believe Propagation Algorithm
+	----------------------------------------------------------------------------
+
+	10/04/2023
+*/
+
+#include "../include/LdpcDecoder.h"
+
+
+void LdpcDecoder_Init(DECODED_FRAME* df, H_MATRIX* H)
+{
+
+
+}
